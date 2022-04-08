497bb447bee2f96c66752e262bb0dce546c778e9
Christopher Guilliams <chris.guilliams@modea.com>
2022-04-08 09:56:49 -0400

    generated publishable angular library

---
+++ libs/ng-webcomponent-example/README.md
@@ -0,0 +1,7 @@
+# ng-webcomponent-example
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test ng-webcomponent-example` to execute the unit tests.
---
+++ libs/ng-webcomponent-example/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'ng-webcomponent-example',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/libs/ng-webcomponent-example',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
